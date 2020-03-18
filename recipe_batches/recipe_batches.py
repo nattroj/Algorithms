7e36870b35bee773a91b9af5b63bb37cc96a3503
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-03-18 12:02:15 -0700

    completed recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,28 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    l = []
+
+    for ingredient, qty in recipe.items():
+        if not ingredients.get(ingredient):
+            return 0
+
+        l.append(ingredients[ingredient] // qty)
+
+    minimum = min(l)
+
+    if minimum < 1:
+        return 0
+
+    return min(l)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
