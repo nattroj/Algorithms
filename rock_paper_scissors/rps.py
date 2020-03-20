b026a830abf67daf9eeb7097f5029a1863816e30
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-03-20 00:35:43 -0700

    completed rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,12 +3,27 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+	results = []
+	moves = ('rock', 'paper', 'scissors')
+
+	def make_round(current_round, n):
+
+		if n == 0:
+			return results.append(current_round)
+		
+		for move in moves:
+			make_round([*current_round, move], n-1)
+		
+	make_round([], n)
+
+	return results
+
+rock_paper_scissors(2)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+	if len(sys.argv) > 1:
+		num_plays = int(sys.argv[1])
+		print(rock_paper_scissors(num_plays))
+	else:
+		print('Usage: rps.py [num_plays]')
